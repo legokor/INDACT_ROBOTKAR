565dcd7614afdf54f17ad8eac1b7f440af255d76
oprezz <antal.marci@gmail.com>
2018-03-20 23:15:16 +0100

    ver1 Gombra 3 motor + végálláskapcsolók

--- MDK-ARM/robotarm/startup_stm32f429xx.d
+++ MDK-ARM/robotarm/startup_stm32f429xx.d
@@ -1,1 +1,1 @@
-.\stepper_motor_1\startup_stm32f429xx.o: startup_stm32f429xx.s
+.\robotarm\startup_stm32f429xx.o: startup_stm32f429xx.s
--- MDK-ARM/stepper_motor_1/startup_stm32f429xx.d
+++
@@ -1,1 +0,0 @@
-.\stepper_motor_1\startup_stm32f429xx.o: startup_stm32f429xx.s
